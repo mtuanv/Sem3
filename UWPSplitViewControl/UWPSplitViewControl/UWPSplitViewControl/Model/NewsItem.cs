6c7e8f2bb85b2ccdfafc4dca0436c1ec1b0ea52b
tuanvmth1911008 <tuanvmth1911008@fpt.edu.vn>
2021-01-29 18:59:21 +0700

    fix

--- UWPSplitViewControl/UWPSplitViewControl/UWPSplitViewControl/Model/NewsItem.cs
+++ UWPSplitViewControl/UWPSplitViewControl/UWPSplitViewControl/Model/NewsItem.cs
@@ -21,6 +21,103 @@ namespace UWPSplitViewControl.Model
 
     public class NewsManager
     {
+        private static List<NewsItem> GetNewsItems()
+        {
+            var items = new List<NewsItem>();
+
+            items.Add(new NewsItem()
+            {
+                Id = 1,
+                Category = "Financial",
+                Headline = "Lorem Ipsum",
+                Subhead = "dolor sit amet",
+                DateLine = "Nunc tristique nec",
+                Image = "Assets/Financial.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 2,
+                Category = "Financial",
+                Headline = "Etiam ac felis viverra",
+                Subhead = "vulputate nisl ac, aliquet nisi",
+                DateLine = "tortor porttitor, eu fermentum ante congue",
+                Image = "Assets/Financial2.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 3,
+                Category = "Financial",
+                Headline = "Lorem Ipsum",
+                Subhead = "dolor sit amet",
+                DateLine = "Nunc tristique nec",
+                Image = "Assets/Financial3.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 4,
+                Category = "Financial",
+                Headline = "Lorem Ipsum",
+                Subhead = "dolor sit amet",
+                DateLine = "Nunc tristique nec",
+                Image = "Assets/Financial4.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 5,
+                Category = "Financial",
+                Headline = "Lorem Ipsum",
+                Subhead = "dolor sit amet",
+                DateLine = "Nunc tristique nec",
+                Image = "Assets/Financial5.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 6,
+                Category = "Food",
+                Headline = "Lorem Ipsum",
+                Subhead = "dolor sit amet",
+                DateLine = "Nunc tristique nec",
+                Image = "Assets/Food1.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 7,
+                Category = "Food",
+                Headline = "Lorem Ipsum",
+                Subhead = "dolor sit amet",
+                DateLine = "Nunc tristique nec",
+                Image = "Assets/Food2.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 8,
+                Category = "Food",
+                Headline = "Lorem Ipsum",
+                Subhead = "dolor sit amet",
+                DateLine = "Nunc tristique nec",
+                Image = "Assets/Food3.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 9,
+                Category = "Food",
+                Headline = "Lorem Ipsum",
+                Subhead = "dolor sit amet",
+                DateLine = "Nunc tristique nec",
+                Image = "Assets/Food4.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 10,
+                Category = "Food",
+                Headline = "Lorem Ipsum",
+                Subhead = "dolor sit amet",
+                DateLine = "Nunc tristique nec",
+                Image = "Assets/Food5.png"
+            });
+
+            return items;
+        }
         public static void GetNews(string category, ObservableCollection<NewsItem> newsItems)
         {
             var allItems = GetNewsItems();
@@ -29,104 +126,7 @@ namespace UWPSplitViewControl.Model
 
             newsItems.Clear();
 
-            fillteredNewsItems.ForEach(prop => newsItems.Add(p));
+            fillteredNewsItems.ForEach(p => newsItems.Add(p));
         }
-    }
-    private static List<NewsItem> GetNewsItems()
-    {
-        var items = new List<NewsItem>();
-
-        items.Add(new NewsItem()
-        {
-            Id = 1,
-            Category = "Financial",
-            Headline = "Lorem Ipsum",
-            Subhead = "dolor sit amet",
-            DateLine = "Nunc tristique nec",
-            Image = "Assets/Financial1.png"
-        });
-        items.Add(new NewsItem()
-        {
-            Id = 2,
-            Category = "Financial",
-            Headline = "Etiam ac felis viverra",
-            Subhead = "vulputate nisl ac, aliquet nisi",
-            DateLine = "tortor porttitor, eu fermentum ante congue",
-            Image = "Assets/Financial2.png"
-        });
-        items.Add(new NewsItem()
-        {
-            Id = 3,
-            Category = "Financial",
-            Headline = "Lorem Ipsum",
-            Subhead = "dolor sit amet",
-            DateLine = "Nunc tristique nec",
-            Image = "Assets/Financial3.png"
-        });
-        items.Add(new NewsItem()
-        {
-            Id = 4,
-            Category = "Financial",
-            Headline = "Lorem Ipsum",
-            Subhead = "dolor sit amet",
-            DateLine = "Nunc tristique nec",
-            Image = "Assets/Financial4.png"
-        });
-        items.Add(new NewsItem()
-        {
-            Id = 5,
-            Category = "Financial",
-            Headline = "Lorem Ipsum",
-            Subhead = "dolor sit amet",
-            DateLine = "Nunc tristique nec",
-            Image = "Assets/Financial5.png"
-        });
-        items.Add(new NewsItem()
-        {
-            Id = 6,
-            Category = "Food",
-            Headline = "Lorem Ipsum",
-            Subhead = "dolor sit amet",
-            DateLine = "Nunc tristique nec",
-            Image = "Assets/Food1.png"
-        });
-        items.Add(new NewsItem()
-        {
-            Id = 7,
-            Category = "Food",
-            Headline = "Lorem Ipsum",
-            Subhead = "dolor sit amet",
-            DateLine = "Nunc tristique nec",
-            Image = "Assets/Food2.png"
-        });
-        items.Add(new NewsItem()
-        {
-            Id = 8,
-            Category = "Food",
-            Headline = "Lorem Ipsum",
-            Subhead = "dolor sit amet",
-            DateLine = "Nunc tristique nec",
-            Image = "Assets/Food3.png"
-        });
-        items.Add(new NewsItem()
-        {
-            Id = 9,
-            Category = "Food",
-            Headline = "Lorem Ipsum",
-            Subhead = "dolor sit amet",
-            DateLine = "Nunc tristique nec",
-            Image = "Assets/Food4.png"
-        });
-        items.Add(new NewsItem()
-        {
-            Id = 10,
-            Category = "Food",
-            Headline = "Lorem Ipsum",
-            Subhead = "dolor sit amet",
-            DateLine = "Nunc tristique nec",
-            Image = "Assets/Food5.png"
-        });
-
-        return items;
     }
 }

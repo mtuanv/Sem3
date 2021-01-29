9aa6dfc8f3003582e25951791b695d5cf1e21628
tuanvmth1911008 <tuanvmth1911008@fpt.edu.vn>
2021-01-29 18:50:30 +0700

    UWP lab4

---
+++ UWP lab4/UWPLab4/UWPLab4/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPLab4.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AvatarPath { get; set; }
+    }
+}
---
+++ UWP lab4/UWPLab4/UWPLab4/Models/Icon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPLab4.Models
+{
+    public class Icon
+    {
+        public string IconPath { get; set; }
+    }
+}
